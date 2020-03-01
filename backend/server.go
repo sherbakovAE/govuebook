652676db5794d9c2ebeab84a22269c8cdee0721e
ScherbakovAE <stack.sherbakov@gmail.com>
2020-03-01 18:19:08 +0300

    Подключение HelloWorld фронтенда на Vue (Vuetify)

--- backend/server.go
+++ backend/server.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	echo "github.com/labstack/echo/v4"
-	"net/http"
 )
 
 func main() {
 
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.Static("/", "frontend/dist")
 	e.Logger.Fatal(e.Start(":1323"))
 }
